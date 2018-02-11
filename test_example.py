05ad3cfd7762fa5f841211eb5b4975033893366d
Illya Petrov <ilya.muromec@gmail.com>
2018-02-11 20:41:09 +0200

    use chrome and chromedriver from apt repo

--- test_example.py
+++ test_example.py
@@ -2,6 +2,7 @@ import pytest
 
 @pytest.fixture
 def chrome_options(chrome_options):
+    chrome_options.binary_location = '/usr/lib/chromium-browser/chromedriver'
     chrome_options.add_argument('--headless')
     return chrome_options
 

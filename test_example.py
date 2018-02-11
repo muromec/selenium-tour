47932469af378f6754d646d728ee8360bf02bced
Illya Petrov <ilya.muromec@gmail.com>
2018-02-11 21:56:22 +0200

    no luck with chrome, try firefox

--- test_example.py
+++ test_example.py
@@ -5,6 +5,11 @@ def chrome_options(chrome_options):
     chrome_options.set_headless()
     return chrome_options
 
+@pytest.fixture
+def firefox_options(firefox_options):
+    firefox_options.add_argument('-headless')
+    return firefox_options
+
 def test_example(selenium):
     selenium.get('http://www.example.com')
 
